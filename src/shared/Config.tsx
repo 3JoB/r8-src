85ac85e2c20729b626ce48b6753aafb8a7b058a2
Zero <54469706+3JoB@users.noreply.github.com>
2023-04-07 08:48:15 +0800

    init

---
+++ src/shared/Config.tsx
@@ -0,0 +1,6 @@
+import './types/config';
+
+export let settings: Config.API = {
+    address: 'http://127.0.0.1',
+    key: '',
+  }
---
+++ src/shared/types/Config.tsx
@@ -0,0 +1,6 @@
+declare namespace Config {
+    interface API {
+        address: string;
+        key: string;
+    }
+}
